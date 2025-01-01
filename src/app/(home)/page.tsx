6df8d45a61c0b68a56c56962cb5c4d18b1c6ecad
AdrianKnapp <adrknapp@gmail.com>
2025-01-01 20:50:25 -0300

    feat(description): added desc

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <>
+    <div className="flex flex-col gap-4">
       <Profile
         imageUrl="https://avatars.githubusercontent.com/u/71047946?v=4"
         name="Adrian Knapp"
@@ -17,37 +17,43 @@ const Home = () => {
           </>
         }
       />
-      <SocialLinks
-        links={[
-          {
-            icon: "linkedin",
-            url: "https://www.linkedin.com/in/adrknapp/",
-            name: "LinkedIn",
-          },
-          {
-            icon: "instagram",
-            url: "https://www.instagram.com/adrknapp/?hl=pt-br",
-            name: "Instagram",
-          },
-          {
-            icon: "github",
-            url: "https://github.com/AdrianKnapp",
-            name: "GitHub",
-          },
-          {
-            icon: "whatsapp",
-            url: "https://wa.me/5553984733103",
-            name: "WhatsApp",
-          },
-          {
-            icon: "email",
-            url: "mailto:adrknapp@gmail.com",
-            name: "E-mail",
-          },
-        ]}
-      />
+      <div className="flex flex-col gap-8">
+        <p className="text-base text-black-default  font-light">
+          With over 6 years of experience in software development, I create
+          solutions that combine performance, scalability, and simplicity.
+        </p>
+        <SocialLinks
+          links={[
+            {
+              icon: "linkedin",
+              url: "https://www.linkedin.com/in/adrknapp/",
+              name: "LinkedIn",
+            },
+            {
+              icon: "instagram",
+              url: "https://www.instagram.com/adrknapp/?hl=pt-br",
+              name: "Instagram",
+            },
+            {
+              icon: "github",
+              url: "https://github.com/AdrianKnapp",
+              name: "GitHub",
+            },
+            {
+              icon: "whatsapp",
+              url: "https://wa.me/5553984733103",
+              name: "WhatsApp",
+            },
+            {
+              icon: "email",
+              url: "mailto:adrknapp@gmail.com",
+              name: "E-mail",
+            },
+          ]}
+        />
+      </div>
       {/* <Credits /> */}
-    </>
+    </div>
   );
 };
 
